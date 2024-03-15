03af5c5a6be687da3e78b2820e64f825bec66904
Doug Wegscheid <dwegscheid@sbcglobal.net>
2024-03-15 17:06:59 -0400

    We can read from the TalonSRX!

--- PyPhoenix5.cpp
+++ PyPhoenix5.cpp
@@ -5,34 +5,13 @@
 
 #include <pybind11/pybind11.h>
 
-#include <chrono>
-#include <iostream>
-#include <string>
-#include <string_view>
-#include <thread>
-#include <unistd.h>
-
-int add(int i, int j) {
-    return i + j;
-}
-
-class Pet
-{
-    public:
-        Pet(const std::string &name, int hunger) : name(name), hunger(hunger) {}
-        ~Pet() {}
-
-        void go_for_a_walk() { hunger++; }
-        const std::string &get_name() const { return name; }
-        int get_hunger() const { return hunger; }
-
-    private:
-        std::string name;
-        int hunger;
-};
 
 void feed_enable(int i) {
-	ctre::phoenix::unmanaged::Unmanaged::FeedEnable(i);
+  ctre::phoenix::unmanaged::Unmanaged::FeedEnable(i);
+}
+
+void set_phoenix_diagnostics_start_time(int i) {
+  ctre::phoenix::unmanaged::Unmanaged::SetPhoenixDiagnosticsStartTime(i);
 }
 
 /*
@@ -41,109 +20,39 @@ void feed_enable(int i) {
  */
 class TalonSRXWrapper
 {
-    public:
-	TalonSRXWrapper(int id) : t{id} {}
-	~TalonSRXWrapper() {}
-	int get_device_id() { return t.GetDeviceID(); }
-	void set_power(double power) { t.Set(ControlMode::PercentOutput, power); }
+  public:
+    TalonSRXWrapper(int id) : t{id} {}
+    ~TalonSRXWrapper() {}
+    int get_device_id() { return t.GetDeviceID(); }
+    void set_power(double power) { t.Set(ControlMode::PercentOutput, power); }
+    double get_output_current() { return t.GetOutputCurrent(); }
+    double get_position() { return t.GetSelectedSensorPosition(); }
+    bool is_fwd_limit_switch_closed() { return t.IsFwdLimitSwitchClosed() != 0; }
+    bool is_rev_limit_switch_closed() { return t.IsRevLimitSwitchClosed() != 0; }
+    void set_brake(bool brake) { t.SetNeutralMode(brake ? NeutralMode::Brake : NeutralMode::Coast); }
 
-    private:
-	TalonSRX t;
+  private:
+    TalonSRX t;
 };
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(PyPhoenix5, m) {
-    // optional module docstring
-    m.doc() = "CTRE Phoenix5 example plugin";
+  // optional module docstring
+  m.doc() = "CTRE Phoenix5 example plugin";
 
-    // define add function
-    m.def("add", &add, "A function which adds two numbers");
+  m.def("feed_enable", &feed_enable, "Feed the watchdog");
 
-    // bindings to Pet class
-    py::class_<Pet>(m, "Pet")
-        .def(py::init<const std::string &, int>())
-        .def("go_for_a_walk", &Pet::go_for_a_walk)
-        .def("get_hunger", &Pet::get_hunger)
-        .def("get_name", &Pet::get_name);
+  m.def("set_phoenix_diagnostics_start_time", &set_phoenix_diagnostics_start_time, "Set the delay for starting the diagnostics server");
 
-    m.def("feed_enable", &feed_enable, "Feed the watchdog");
-
-    py::class_<TalonSRXWrapper>(m, "TalonSRX")
-	.def(py::init<int>())
-	.def("get_device_id", &TalonSRXWrapper::get_device_id)
-	.def("set_power", &TalonSRXWrapper::set_power);
+  py::class_<TalonSRXWrapper>(m, "TalonSRX")
+    .def(py::init<int>())
+    .def("get_device_id", &TalonSRXWrapper::get_device_id)
+    .def("set_power", &TalonSRXWrapper::set_power)
+    .def("get_output_current", &TalonSRXWrapper::get_output_current)
+    .def("get_position", &TalonSRXWrapper::get_position)
+    .def("is_fwd_limit_switch_closed", &TalonSRXWrapper::is_fwd_limit_switch_closed)
+    .def("is_rev_limit_switch_closed", &TalonSRXWrapper::is_rev_limit_switch_closed)
+    .def("set_brake", &TalonSRXWrapper::set_brake)
+    ;
 }
-
-
-
-#define Phoenix_No_WPI // remove WPI dependencies
-#include "ctre/Phoenix.h"
-#include "ctre/phoenix/platform/Platform.hpp"
-#include "ctre/phoenix/unmanaged/Unmanaged.h"
-#include <chrono>
-#include <iostream>
-#include <string>
-#include <thread>
-#include <unistd.h>
-
-using namespace ctre::phoenix;
-using namespace ctre::phoenix::platform;
-using namespace ctre::phoenix::motorcontrol;
-using namespace ctre::phoenix::motorcontrol::can;
-
-/* make some talons for drive train */
-std::string interface = "can0";
-// TalonSRX talLeft(1, interface); //Use the specified interface
-TalonSRX talLeft(1);
-
-/** simple wrapper for code cleanup */
-void sleepApp(int ms)
-{
-	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-}
-
-void goFor1S(double power)
-{
-	for (int i = 0; i < 50; i++) {
-		talLeft.Set(ControlMode::PercentOutput, power);
-		ctre::phoenix::unmanaged::Unmanaged::FeedEnable(100);
-
-		double current = talLeft.GetOutputCurrent();
-		double position = talLeft.GetSelectedSensorPosition(0);
-		int f = talLeft.IsFwdLimitSwitchClosed();
-		int b = talLeft.IsRevLimitSwitchClosed();
-		printf("output current = %f, position = %f, f=%d, b=%d\n", current, position, f, b);
-
-		/* loop yield for a bit */
-		sleepApp(20);
-	}
-}
-
-
-
-int main() {	
-	// Comment out the call if you would rather use the automatically running diag-server, note this requires uninstalling diagnostics from Tuner. 
-	// c_SetPhoenixDiagnosticsStartTime(-1); // disable diag server, instead we will use the diag server stand alone application that Tuner installs
-
-	printf ("device id = %d", talLeft.GetDeviceID());
-	while (true) {
-		talLeft.SetNeutralMode(NeutralMode::Brake);
-		goFor1S(0.2);
-		goFor1S(0.0);
-		goFor1S(-0.2);
-		goFor1S(0.0);
-		talLeft.SetNeutralMode(NeutralMode::Coast);
-		goFor1S(0.2);
-		goFor1S(0.0);
-		goFor1S(-0.2);
-		goFor1S(0.0);
-		while (true) {
-			goFor1S(0.0);
-		}
-	}
-	talLeft.Set(ControlMode::PercentOutput, 0.0);
-
-	return 0;
-}
-
--- test.py
+++ test.py
@@ -2,15 +2,27 @@ import PyPhoenix5
 import time
 
 def test_SRX():
-    talon = PyPhoenix5.TalonSRX(1)
-    print(talon)
-    print(talon.get_device_id())
-    talon.set_power(0.5)
-    for i in range(100):
+  PyPhoenix5.set_phoenix_diagnostics_start_time(0)
+  talon = PyPhoenix5.TalonSRX(1)
+  print(talon)
+  print(talon.get_device_id())
+  if True:
+    talon.set_power(0.1)
+    if True:
+      for i in range(100):
         PyPhoenix5.feed_enable(100)
         time.sleep(0.05)
+        # print(talon.get_position(), talon.get_output_current())
     talon.set_power(0.0)
+  while True:
+    PyPhoenix5.feed_enable(200)
+    time.sleep(0.05)
+    print(talon.get_position(), talon.is_fwd_limit_switch_closed(), talon.is_rev_limit_switch_closed())
+
 
 if __name__ == '__main__':
+  try:
     test_SRX()
+  except KeyboardInterrupt:
+    pass
 
